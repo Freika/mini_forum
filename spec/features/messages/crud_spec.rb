21d97d5140bb6d1b3b451b3b885873bf5d4ab215
Eugene Burmakin <iamfrey@gmail.com>
2017-06-06 19:58:25 +0700

    Update tests.

--- spec/features/messages/crud_spec.rb
+++ spec/features/messages/crud_spec.rb
@@ -4,32 +4,29 @@ feature 'Message', js: true do
   before(:each) do
     login_as user, scope: :user
     visit root_path
+    create_message
   end
 
   scenario 'creating' do
-    create_message
-
-    expect(page).to have_content('MyString')
+    expect(page).to have_content('Uniq Message')
   end
 
   feature 'existing' do
-    before(:each) { create_message }
-
     scenario 'deleting' do
       click_link '[x]'
 
-      expect(page).to_not have_content('MyString')
+      expect(page).to_not have_content('Uniq Message')
     end
 
     scenario 'editing' do
       click_link '[Edit]'
 
       within "##{dom_id(Message.last)}" do
-        fill_in 'message_content', with: 'Updated MyString'
+        fill_in 'message_content', with: 'Updated Uniq Message'
         click_button 'Post'
       end
 
-      expect(page).to have_content('Updated MyString')
+      expect(page).to have_content('Updated Uniq Message')
     end
   end
 
@@ -38,7 +35,7 @@ feature 'Message', js: true do
   def create_message
     click_link 'New message'
 
-    fill_in 'message_content', with: 'MyString'
+    fill_in 'message_content', with: 'Uniq Message'
     click_button 'Post'
   end
 end
