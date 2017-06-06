8bd802a675456bef4b53063f221e83608166cc89
Eugene Burmakin <iamfrey@gmail.com>
2017-06-07 02:58:51 +0700

    Add error rescue.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,8 +3,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      # Disabled until Facebook approves application.
-      # user_friends(request.env['omniauth.auth']) unless Rails.env.test?
+      user_friends(request.env['omniauth.auth']) unless Rails.env.test?
 
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
@@ -31,5 +30,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     names = friends.map { |f| f['name'] }
 
     ReadCache.redis.sadd("users:friends:#{@user.id}", names)
+  rescue
+    {}
   end
 end
