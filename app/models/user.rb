82ee534cbffb4f0e557189511ce02d5727c4df31
Eugene Burmakin <iamfrey@gmail.com>
2017-06-07 03:00:24 +0700

    Use email if no name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,10 @@ class User < ApplicationRecord
   end
 
   def full_name
-    "#{first_name} #{last_name}"
+    if first_name || last_name
+      "#{first_name} #{last_name}"
+    else
+      email
+    end
   end
 end
